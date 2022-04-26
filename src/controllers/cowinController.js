53037d0f5b421a781d4eee20bf3c791650f76899
BAravind85 <arubrp09@gmail.com>
2022-04-26 12:42:51 +0530

    aru

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,7 +1,8 @@
-let axios = require("axios")
+let axios = require("axios");
+const { get } = require("express/lib/response");
 
 
-let getStates = async function (req, res) {
+let getStates = async function(req, res) {
 
     try {
         let options = {
@@ -12,15 +13,14 @@ let getStates = async function (req, res) {
         console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
 
-let getDistricts = async function (req, res) {
+let getDistricts = async function(req, res) {
     try {
         let id = req.params.stateId
         let options = {
@@ -31,36 +31,34 @@ let getDistricts = async function (req, res) {
         console.log(result)
         let data = result.data
         res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
-let getByPin = async function (req, res) {
+let getByPin = async function(req, res) {
     try {
-        let pin = req.query.pincode
+        let pincode = req.query.pincode
         let date = req.query.date
-        console.log(`query params are: ${pin} ${date}`)
+        console.log(`query params are: ${pincode} ${date}`)
         var options = {
             method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${date}`
         }
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
-let getOtp = async function (req, res) {
+let getOtp = async function(req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -71,15 +69,92 @@ let getOtp = async function (req, res) {
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
     }
-    catch (err) {
+}
+let getbydistrict = async function(req, res) {
+    try {
+        let district_id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${district_id} ${date}`)
+
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+let getwhether = async function(req, res) {
+    try {
+
+        let appid = req.query.appid
+        console.log(`query params are: ${appid}`)
+
+        let options = {
+            method: "get",
+            url: ` http://api.openweathermap.org/data/2.5/weather?q=delhi&appid=${appid}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+let getmemes = async function(req, res) {
+    try {
+
+        let options = {
+            method: "get",
+            url: ` https://api.imgflip.com/get_memes`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+let postmemes = async function(req, res) {
+    try {
+        let template_id = req.query.template_id
+        let text0 = req.query.text0
+        let text1 = req.query.text1
+        let username = req.query.username
+        let password = req.query.password
+
+        let options = {
+            method: "post",
+            url: ` https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    } catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
 
-
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getbydistrict = getbydistrict
+module.exports.getwhether = getwhether
+module.exports.getmemes = getmemes
+module.exports.postmemes = postmemes
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const CowinController = require("../controllers/cowinController")
 
 
 
-router.get("/test-me", function (req, res) {
+router.get("/test-me", function(req, res) {
     res.send("My first ever api!")
 })
 
@@ -12,6 +12,10 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
+router.get("/cowin/getbydistrict", CowinController.getbydistrict)
+router.get("/getwhether", CowinController.getwhether)
+router.get("/getmemes", CowinController.getmemes)
+router.post("/postmemes", CowinController.postmemes)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
